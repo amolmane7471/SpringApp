fd82119149106532be18f0e2421c8450adc26b1e
amolmane7471 <maneamol3233@gmail.com>
2022-11-01 12:43:36 +0530

    [Amol] : added UC1 helloWorld spring app to display msg using STS

---
+++ src/main/java/com/bridgelabz/controller/HelloRestController.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.controller;
+import com.bridgelabz.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    @RequestMapping(value = {"","/","/home"})
+    public String sayHello(){
+        return "Hello From BridgeLabz";
+    }
+
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name){
+        return "Hello" + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "Hello" + name + "!";
+    }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user){
+        return "Hello" + user.getFirstname() + user.getLastName() + "!";
+    }
+
+    @PutMapping("/put/{firstname}")
+    public String sayHello(@PathVariable String firstname,
+                           @RequestParam(value = "lastName") String lastName){
+        return "Hello" + firstname+ " "+lastName + "!";
+    }
+
+}
---
+++ src/main/java/com/bridgelabz/model/User.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.model;
+
+public class User {
+    private String firstname;
+    private String lastName;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
