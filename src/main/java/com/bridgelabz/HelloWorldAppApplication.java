0988c47d71de2fd1ab182bf6a249572f063f6aa3
amolmane7471 <maneamol3233@gmail.com>
2022-11-03 16:26:06 +0530

    [Amol] : added UC1 to create Rest Controller to demonstrate the various http methods

--- src/main/java/com/bridgelabz/HelloWorldAppApplication.java
+++ src/main/java/com/bridgelabz/HelloWorldAppApplication.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.helloworldapp;
+package com.bridgelabz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldAppApplication {
     public static void main(String[] args) {
-        System.out.println("Hello From Bridgelabz");
+    //    System.out.println("Hello From Bridgelabz");
         SpringApplication.run(HelloWorldAppApplication.class, args);
     }
 
